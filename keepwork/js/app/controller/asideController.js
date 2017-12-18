131a8ac21f1284f1ed5132856e6e33c1a9622677
Xuntian <li.zq@foxmail.com>
2017-12-18 18:41:12 +0800

    admin page

--- keepwork/js/app/controller/asideController.js
+++ keepwork/js/app/controller/asideController.js
@@ -6,14 +6,7 @@ define([
 	'text!html/aside.html',
 ], function (app, mdwiki, util, htmlContent) {
     app.registerController("asideController",['$scope', function ($scope) {
-		$scope.message = "this is a test";
-
-		$scope.showManagerView = function(){
-			window.location.href = "/dashboard/admin";
-		}
-		$scope.showManagerView = function(){
-			window.location.href = "/dashboard/admin";
-		}
+		$scope.message = "this is aside panel";
 
 		$scope.showGivenView = function(viewName){
 			window.location.href = "/dashboard/" + viewName;
